7975c83ad512694679bc46d79a260d7234d4ac95
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 02:00:44 +0530

    Update bans.py

--- Senku/modules/bans.py
+++ Senku/modules/bans.py
@@ -14,10 +14,10 @@ from telegram.utils.helpers import mention_html
 from typing import Optional, List
 from telegram import TelegramError
 
-import EmikoRobot.modules.sql.users_sql as sql
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.filters import CustomFilters
-from EmikoRobot import (
+import Senku.modules.sql.users_sql as sql
+from Senku.modules.disable import DisableAbleCommandHandler
+from Senku.modules.helper_funcs.filters import CustomFilters
+from Senku import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -27,7 +27,7 @@ from EmikoRobot import (
     WOLVES,
     dispatcher,
 )
-from EmikoRobot.modules.helper_funcs.chat_status import (
+from Senku.modules.helper_funcs.chat_status import (
     user_admin_no_reply,
     bot_admin,
     can_restrict,
@@ -40,9 +40,9 @@ from EmikoRobot.modules.helper_funcs.chat_status import (
     can_delete,
     dev_plus,
 )
-from EmikoRobot.modules.helper_funcs.extraction import extract_user_and_text
-from EmikoRobot.modules.helper_funcs.string_handling import extract_time
-from EmikoRobot.modules.log_channel import gloggable, loggable
+from Senku.modules.helper_funcs.extraction import extract_user_and_text
+from Senku.modules.helper_funcs.string_handling import extract_time
+from Senku.modules.log_channel import gloggable, loggable
 
 
 
