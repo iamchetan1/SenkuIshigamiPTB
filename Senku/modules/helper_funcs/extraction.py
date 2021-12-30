6e5187f7d0fef8cf2db96c33f3329e464336f885
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 12:49:31 +0530

    Update extraction.py

--- Senku/modules/helper_funcs/extraction.py
+++ Senku/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from EmikoRobot import LOGGER
-from EmikoRobot.modules.users import get_user_id
+from Senku import LOGGER
+from Senku.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
