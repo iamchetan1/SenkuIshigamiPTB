036edf057ebd13bc86f845ab9d81276e8c844804
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 14:00:18 +0530

    Update __init__.py

--- Senku/__init__.py
+++ Senku/__init__.py
@@ -297,7 +297,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from Senku import
+from Senku.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
