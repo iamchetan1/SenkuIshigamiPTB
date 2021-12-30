361e719e9c30bb73c8f0e46c00a8871746eef17e
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 12:34:55 +0530

    Update connection_sql.py

--- Senku/modules/sql/connection_sql.py
+++ Senku/modules/sql/connection_sql.py
@@ -2,7 +2,7 @@ import threading
 import time
 from typing import Union
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
-from EmikoRobot.modules.sql import SESSION, BASE
+from Senku.modules.sql import SESSION, BASE
 from sqlalchemy.sql.sqltypes import BigInteger
 
 
