c93175660c052dc4096b7f5de75fdbee6dd6a779
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 14:30:51 +0530

    Create ExtraGifs_strings.py

---
+++ Senku/modules/ExtraGifs_strings.py
@@ -0,0 +1,77 @@
+HUGBG = [
+    'https://tenor.com/bieym.gif',
+    'https://tenor.com/LUqw.gif',
+    'https://tenor.com/blkN3.gif',
+    'https://tenor.com/buIr7.gif',
+    'https://tenor.com/1jOu.gif',
+    'https://tenor.com/2eJl.gif',
+    'https://tenor.com/xNY7.gif',
+    'https://tenor.com/2SQu.gif',
+    'https://tenor.com/Mkwx.gif',
+]
+
+
+HUGGB = [
+    'https://tenor.com/be3CQ.gif',
+    'https://tenor.com/bmI9Y.gif',
+    'https://tenor.com/bgyzF.gif',
+    'https://tenor.com/baHmj.gif',
+    'https://tenor.com/1jRF.gif',
+    'https://tenor.com/bbJq2.gif',
+    'https://tenor.com/1jOu.gif',
+    'https://tenor.com/2eJl.gif',
+    'https://tenor.com/bAuyA.gif',
+    'https://tenor.com/Mkwx.gif',
+    'https://tenor.com/47UW.gif',
+]
+
+HUGGG = [
+    'https://tenor.com/QWw1.gif',
+    'https://tenor.com/UDdM.gif',
+    'https://tenor.com/bkBq0.gif',
+    'https://tenor.com/brIGP.gif',
+    'https://tenor.com/beqF0.gif',
+    'https://tenor.com/bfHgd.gif',
+    'https://tenor.com/bpgad.gif',
+    'https://tenor.com/buoQa.gif',
+    'https://tenor.com/baHmu.gif',
+    'https://tenor.com/2At9.gif',
+    'https://tenor.com/biBl3.gif',
+]
+
+HUGBB = [
+    'https://tenor.com/btrDT.gif',
+    'https://tenor.com/uIU6.gif',
+    'https://tenor.com/47ZC.gif',
+    'https://tenor.com/bEm6t.gif',
+    'https://tenor.com/9wRK.gif',
+    'CgACAgQAAx0CUfVQawABFkiZYXg9AY9TBqOyFdA4REbB_0GQRogAAv4CAAK1iFVSFKFmbvH86-IhBA',
+]
+
+
+PAT = [
+    'CgACAgQAAx0CUfVQawABFkYXYXgze7cAAYvTZhnxXJzAOnik7iENAAIXBwACSFo4U36Foq4FSJl1IQQ',
+    'CgACAgQAAx0CUfVQawABFkYsYXgztx66vITm_1QM2Ea8hdHHp2cAAokGAAJV-QABULnRMnArZw11IQQ',
+    'CgACAgQAAx0CUfVQawABFkYuYXgzwDFKVVNlWaLd1KW9dslhQGAAAnQCAAJVN3xQTH3ala_poNohBA',
+    'CgACAgQAAx0CUfVQawABFkY3YXgz_9nFVJLJbTU0vXCCHSpQlfgAAtQEAAIMXylSH4kSJYcjGMwhBA',
+    'CgACAgQAAx0CUfVQawABFkY4YXg0Bl5J22RRtFQNJVzLWjwjW00AAn0EAAItXdhT5ALlBMsf4TYhBA',
+    'CgACAgQAAx0CUfVQawABFkY5YXg0CpbM77cyiASx9zszmr09XOIAAokGAAL3RWBS59KWukOLi6IhBA',
+    'CgACAgQAAx0CUfVQawABFkY8YXg0KWtxZszY61MJk85Pb0aeSuAAAjsBAAKDrjVQUPah291XkUQhBA',
+    'CgACAgQAAx0CUfVQawABFkZaYXg0rx_6YcdrQmzd4c8AATR1dNCEAALRCgACYrI5Uu0ZMJ0uers1IQQ',
+    'CgACAgQAAx0CUfVQawABFkZcYXg0sJtvO98meJYOUPw9_Z-1rg4AAgcCAAKIEpRQpnwbg4t0oWUhBA',
+    'CgACAgQAAx0CUfVQawABFkZeYXg0sRjFg5SYqa28aTneaIXjrXQAAsIGAALqXchQGnmklJRFB9ghBA',
+    'CgACAgQAAx0CUfVQawABFkaEYXg1E55u_G7wvQN4WPHP9ZOWDRYAAokFAAJkVMFTfnACUmF1NjwhBA',
+    'CgACAgQAAx0CUfVQawABFkZ0YXg075mNZp2F9BRrjT1Cov79hzQAArIFAAKYEGhRUtQh9HcMFukhBA',
+    'CgACAgQAAx0CUfVQawABFkZpYXg0zlf3kj9mpkI4fzNlkCQj9VsAAigJAAI1-GhQvzVVjg85ua8hBA',
+    'CgACAgQAAx0CUfVQawABFkZiYXg0wzfrrqEO-iq_ycKKM5hpw6cAAtIKAAJisjlSbH5m93EbhZQhBA',
+]
+
+
+KISS = [
+    'CgACAgQAAx0CUfVQawABFkZEYXhBriDxba75jMi2eMECObesj9EAAocFAAJkVMFTEOXjNWXT_74hBA',
+    'CgACAgQAAx0CUfVQawABFkZFYXhBtoNpcqKgM8tepBsHXNVJrxgAAo8GAAKjSUlSpQFihMRlJRAhBA',
+    'CgACAgQAAx0CUfVQawABFkZKYXhBvug0SOXEsWeFlAvWRETLJ3oAAtULAAJ2N-BRItoLbSi1nBwhBA',
+    'CgACAgUAAx0CXfPEtQACBSJheqg9y00R9BN7-EyjulnoIGy9DwACKQQAAjPP2FcoVk8zzrONPyEE',
+    'CgACAgUAAx0CXfPEtQACBSNheqg9XH9EL8nAkM5v26DagzMX_wACjQQAAup_2Vc60jQ_doRC6yEE',
+    'CgACAgUAAx0CXfPEtQACBSRheqg9NiicK8G9B-zVPrljzcnkfQACjgQAAup_2VfQQX1_rIBPvyEE',
+]
