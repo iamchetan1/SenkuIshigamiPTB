50e1c0f3c41e9844348720717fcb8c4982ff176d
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 12:33:57 +0530

    Update blsticker_sql.py

--- Senku/modules/sql/blsticker_sql.py
+++ Senku/modules/sql/blsticker_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func, Integer
 
-from EmikoRobot.modules.sql import BASE, SESSION
+from Senku.modules.sql import BASE, SESSION
 
 
 class StickersFilters(BASE):
