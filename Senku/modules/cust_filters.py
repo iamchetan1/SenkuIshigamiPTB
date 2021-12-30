3d40460d92c8e57e4c0d55886ddd9b1e1136577c
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 14:16:16 +0530

    Update cust_filters.py

--- Senku/modules/cust_filters.py
+++ Senku/modules/cust_filters.py
@@ -13,23 +13,23 @@ from telegram.ext import (
     Filters,
 )
 from telegram.utils.helpers import mention_html, escape_markdown
-from EmikoRobot import dispatcher, LOGGER, DRAGONS
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from EmikoRobot.modules.helper_funcs.chat_status import user_admin
-from EmikoRobot.modules.helper_funcs.extraction import extract_text
-from EmikoRobot.modules.helper_funcs.filters import CustomFilters
-from EmikoRobot.modules.helper_funcs.misc import build_keyboard_parser
-from EmikoRobot.modules.helper_funcs.msg_types import get_filter_type
-from EmikoRobot.modules.helper_funcs.string_handling import (
+from Senku import dispatcher, LOGGER, DRAGONS
+from Senku.modules.disable import DisableAbleCommandHandler
+from Senku.modules.helper_funcs.handlers import MessageHandlerChecker
+from Senku.modules.helper_funcs.chat_status import user_admin
+from Senku.modules.helper_funcs.extraction import extract_text
+from Senku.modules.helper_funcs.filters import CustomFilters
+from Senku.modules.helper_funcs.misc import build_keyboard_parser
+from Senku.modules.helper_funcs.msg_types import get_filter_type
+from Senku.modules.helper_funcs.string_handling import (
     split_quotes,
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
-from EmikoRobot.modules.sql import cust_filters_sql as sql
-from EmikoRobot.modules.connection import connected
-from EmikoRobot.modules.helper_funcs.alternate import send_message, typing_action
+from Senku.modules.sql import cust_filters_sql as sql
+from Senku.modules.connection import connected
+from Senku.modules.helper_funcs.alternate import send_message, typing_action
 
 HANDLER_GROUP = 10
 
