af270382d5beda22918b03dd25509e4f1d690245
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 17:57:23 +0530

    Update __init__.py

--- Senku/modules/sql/__init__.py
+++ Senku/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from Senku import DB_URL
+from Senku import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
