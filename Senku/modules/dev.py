5ba7d67ebd3c8211b2cd8e266bcceb9e8eaafa9e
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 12:24:00 +0530

    Update dev.py

--- Senku/modules/dev.py
+++ Senku/modules/dev.py
@@ -5,10 +5,10 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import EmikoRobot
+import Senku
 
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.helper_funcs.chat_status import dev_plus
+from Senku import dispatcher
+from Senku.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler
