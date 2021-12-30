08ddd3666f805d664bf035947e7c082611269b21
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 12:59:36 +0530

    Update permissions.py

--- Senku/utils/permissions.py
+++ Senku/utils/permissions.py
@@ -1,9 +1,9 @@
 from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
-from EmikoRobot import pbot as app
-from EmikoRobot import DRAGONS, DEV_USERS, WOLVES, DEMONS, TIGERS
-from EmikoRobot.utils.adminperms import member_permissions
+from Senku import pbot as app
+from Senku import DRAGONS, DEV_USERS, WOLVES, DEMONS, TIGERS
+from Senku.utils.adminperms import member_permissions
 
 SUDO = DRAGONS, DEV_USERS, WOLVES, DEMONS, TIGERS
 
