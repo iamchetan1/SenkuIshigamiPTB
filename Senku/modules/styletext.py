5168f53139f14b9fa93a10bce3620fbca97cf11a
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 11:57:24 +0530

    Update styletext.py

--- Senku/modules/styletext.py
+++ Senku/modules/styletext.py
@@ -1,6 +1,6 @@
-from EmikoRobot  import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.alternate import typing_action
+from Senku import dispatcher
+from Senku.modules.disable import DisableAbleCommandHandler
+from Senku.modules.helper_funcs.alternate import typing_action
 from telegram import ParseMode
 from telegram.ext import run_async
 
