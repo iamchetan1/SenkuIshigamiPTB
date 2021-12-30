a7ec71ab918b074595e570fd3b9b67c6e721ca82
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 12:02:19 +0530

    Update purge.py

--- Senku/modules/purge.py
+++ Senku/modules/purge.py
@@ -2,8 +2,8 @@ import asyncio
 import time
 from telethon import events
 
-from EmikoRobot import telethn
-from EmikoRobot.modules.helper_funcs.telethn.chatstatus import (
+from Senku import telethn
+from Senku.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
 )
