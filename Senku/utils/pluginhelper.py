b988e8358afa3c1998f070e44b20a58d24e253b7
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 12:59:57 +0530

    Update pluginhelper.py

--- Senku/utils/pluginhelper.py
+++ Senku/utils/pluginhelper.py
@@ -13,8 +13,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from EmikoRobot import OWNER_ID, SUPPORT_CHAT
-from EmikoRobot import pbot
+from Senku import OWNER_ID, SUPPORT_CHAT
+from Senku import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
