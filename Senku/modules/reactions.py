97e44ce752272e0ae2b91c919dd4a95e2d73256c
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 14:23:11 +0530

    Update reactions.py

--- Senku/modules/reactions.py
+++ Senku/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from Senku import dispatcher
+from Senku.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext
 
