21ab10ba675386f92d320a6a6244866aca9eaf60
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-12-30 12:24:48 +0530

    Update dbcleanup.py

--- Senku/modules/dbcleanup.py
+++ Senku/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import EmikoRobot.modules.sql.global_bans_sql as gban_sql
-import EmikoRobot.modules.sql.users_sql as user_sql
-from EmikoRobot import DEV_USERS, OWNER_ID, dispatcher
-from EmikoRobot.modules.helper_funcs.chat_status import dev_plus
+import Senku.modules.sql.global_bans_sql as gban_sql
+import Senku.modules.sql.users_sql as user_sql
+from Senku import DEV_USERS, OWNER_ID, dispatcher
+from Senku.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
