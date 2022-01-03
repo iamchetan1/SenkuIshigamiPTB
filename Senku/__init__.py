f6f656b699b3a4d7ff0e6daddb50978dafe16e14
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2022-01-03 14:42:50 +0530

    Update __init__.py

--- Senku/__init__.py
+++ Senku/__init__.py
@@ -230,12 +230,7 @@ aiohttpsession = ClientSession()
 print("[INFO]: INITIALIZING ARQ CLIENT")
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 pbot = Client("SenkuPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
-ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
-try:
-    ubot2.start()
-except BaseException:
-    print("Userbot Error! Have you added a STRING_SESSION in deploying??")
-    sys.exit(1)
+
 
 
 
